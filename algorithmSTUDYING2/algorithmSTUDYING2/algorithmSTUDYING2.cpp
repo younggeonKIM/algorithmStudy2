a06c270985e97d5b9d9f46540069760a66e60969
영건 김 <young3702@naver.com>
2024-03-12 22:24:00 +0900

    20240312_algorithmStudy(selectionSorting)_added

--- algorithmSTUDYING2/algorithmSTUDYING2/algorithmSTUDYING2.cpp
+++ algorithmSTUDYING2/algorithmSTUDYING2/algorithmSTUDYING2.cpp
@@ -2,10 +2,13 @@
 //
 
 #include <iostream>
+#include "selectionSorting.cpp"
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
+    sorting();
 }
 
 // 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
